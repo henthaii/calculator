208494eb7885394083669b1f6fe7108df7096ce8
henthaii <200550625+henthaii@users.noreply.github.com>
2026-01-24 16:20:22 -0600

    updating java file with more functions. completed multiply and divide. added in sum function. added in all const for numbers.

--- calculator.js
+++ calculator.js
@@ -1,16 +1,55 @@
-const add = function (a,b) {
+//Functions
+const add = (a,b) => {
     return a + b;
 }
 
-const subtract = function (a,b) {
+const subtract = (a,b) => {
     return a - b;
 }
 
+const multiply = (a,b) => {
+    return a * b;
+}
+
+const divide = (a,b) => {
+    return a / b;
+}
+
+
+
+//Defining the consts
 const one = document.getElementById('one').addEventListener("click",inputDisplay);
 console.log(one);
 
-function inputDisplay () {
-    const display = document.getElementById('input');
-    display.innerHTML = '';
-    console.log("one");
-}
+const two = document.getElementById('two').addEventListener("click",inputDisplay);
+console.log(two);
+
+const three = document.getElementById('three').addEventListener("click",inputDisplay);
+console.log(three);
+
+const four = document.getElementById('four').addEventListener("click",inputDisplay);
+console.log(four);
+
+const five = document.getElementById('five').addEventListener("click",inputDisplay);
+console.log(five);
+
+const six = document.getElementById('six').addEventListener("click",inputDisplay);
+console.log(six);
+
+const seven = document.getElementById('seven').addEventListener("click",inputDisplay);
+console.log(seven);
+
+const eight = document.getElementById('eight').addEventListener("click",inputDisplay);
+console.log(eight);
+
+const nine = document.getElementById('nine').addEventListener("click",inputDisplay);
+console.log(nine);
+
+const zero = document.getElementById('zero').addEventListener("click",inputDisplay);
+console.log(zero);
+
+// function inputDisplay () {
+//     const display = document.getElementById('input');
+//     display.innerHTML = '';
+//     console.log("one");
+// }
